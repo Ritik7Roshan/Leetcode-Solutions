24d18150d22309c7552e2552554d6017c59534be
Ritik7Roshan <86067297+Ritik7Roshan@users.noreply.github.com>
2022-09-06 11:49:49 +0530

    Time: 4 ms (49.90%), Space: 8.8 MB (64.29%) - LeetHub

--- 814-binary-tree-pruning/814-binary-tree-pruning.cpp
+++ 814-binary-tree-pruning/814-binary-tree-pruning.cpp
@@ -9,27 +9,38 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+//public:
+//     TreeNode* pruneTree(TreeNode* root) 
+//     {
+//         return containsOne(root) ? root:NULL;
+//     }
+    
+//     public:
+//     bool containsOne(TreeNode* node)
+//     {
+//         if(node==NULL) return false;
+        
+//         bool leftContainsOne= containsOne(node->left);
+        
+//         bool rightContainsOne= containsOne(node->right);
+        
+//         if(!leftContainsOne) node->left=NULL;
+        
+//         if(!rightContainsOne) node->right=NULL;
+        
+//         return node->val ==1 || leftContainsOne || rightContainsOne;
+        
+//         }
+
+
 class Solution {
 public:
-    TreeNode* pruneTree(TreeNode* root) 
-    {
-        return containsOne(root) ? root:NULL;
+    TreeNode* pruneTree(TreeNode* root){
+        if(!root) return NULL;
+        root->left=pruneTree(root->left);
+        root->right=pruneTree(root->right);
+        if(! root->left && !root->right && root->val==0) return NULL;
+        return root;
     }
-    
-    public:
-    bool containsOne(TreeNode* node)
-    {
-        if(node==NULL) return false;
-        
-        bool leftContainsOne= containsOne(node->left);
-        
-        bool rightContainsOne= containsOne(node->right);
-        
-        if(!leftContainsOne) node->left=NULL;
-        
-        if(!rightContainsOne) node->right=NULL;
-        
-        return node->val ==1 || leftContainsOne || rightContainsOne;
-        
-        }
 };
