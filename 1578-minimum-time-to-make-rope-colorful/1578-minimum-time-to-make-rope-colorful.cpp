30f2206588d98fca3f520932727a56895b8a973b
Ritik7Roshan <86067297+Ritik7Roshan@users.noreply.github.com>
2022-10-03 23:46:34 +0530

    Time: 371 ms (23.05%), Space: 95.3 MB (84.72%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
     int minCost(string colors, vector<int>& neededTime) {
-        // Initalize two pointers i, j.
-        int totalTime = 0;
-        int i = 0, j = 0;
-        
-        while (i < neededTime.size() && j < neededTime.size()) {
-            int currTotal = 0, currMax = 0;
-            
-            // Find all the balloons having the same color as the 
-            // balloon indexed at i, record the total removal time 
-            // and the maximum removal time.
-            while (j < neededTime.size() && colors[i] == colors[j]) {
-                currTotal += neededTime[j];
-                currMax = max(currMax, neededTime[j]);
-                j++;
+        int totalTime=0, currMaxTime=0;
+        for(int i=0; i<colors.size(); ++i)
+        {
+            //If this balloon is the first ballon of a new group set the currMaxTime as 0;
+            if(i>0 && colors[i]!=colors[i-1]){
+                currMaxTime =0;
             }
-            
-            // Once we reach the end of the current group, add the cost of 
-            // this group to total_time, and reset two pointers.
-            totalTime += currTotal - currMax;
-            i = j;
+            // Increment totalTime by the smaller one.
+            // Update currMaxTime as the larger one.
+            totalTime += min(currMaxTime, neededTime[i]);
+            currMaxTime = max(currMaxTime, neededTime[i]);
         }
+        // Return totalTime as the minimum removal time.
         return totalTime;
     }
 };
