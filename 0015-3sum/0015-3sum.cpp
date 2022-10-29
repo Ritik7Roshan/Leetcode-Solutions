8169ad1ea2336926a0372c4c8cdc1990a128f422
Ritik7Roshan <86067297+Ritik7Roshan@users.noreply.github.com>
2022-10-29 17:04:40 +0530

    Time: 141 ms (69.34%), Space: 21 MB (47.08%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,31 +1,48 @@
 class Solution {
 public:
-     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin() , nums.end());     //Sorted Array
-        if(nums.size() < 3){                // Base Case 1
-            return {};
-        }
-        if(nums[0] > 0){                    // Base Case 2
-            return {};
-        }
-        unordered_map<int , int> hashMap;
-        for(int i = 0 ; i < nums.size() ; ++i){     //Hashing of Indices
-            hashMap[nums[i]] = i;
-        }
-        vector<vector<int>> answer;
-        for(int i = 0 ; i < nums.size() - 2 ; ++i){      
-            if(nums[i] > 0){     
-                break;
+    vector<vector<int>> threeSum(vector<int>& num) {
+        vector<vector<int> > res;
+
+    std::sort(num.begin(), num.end());
+
+    for (int i = 0; i < num.size(); i++) {
+        
+        int target = -num[i];
+        int front = i + 1;
+        int back = num.size() - 1;
+
+        while (front < back) {
+
+            int sum = num[front] + num[back];
+            
+            // Finding answer which start from number num[i]
+            if (sum < target)
+                front++;
+
+            else if (sum > target)
+                back--;
+
+            else {
+                vector<int> triplet = {num[i], num[front], num[back]};
+                res.push_back(triplet);
+                
+                // Processing duplicates of Number 2
+                // Rolling the front pointer to the next different number forwards
+                while (front < back && num[front] == triplet[1]) front++;
+
+                // Processing duplicates of Number 3
+                // Rolling the back pointer to the next different number backwards
+                while (front < back && num[back] == triplet[2]) back--;
             }
-            for(int j = i + 1 ; j < nums.size() - 1 ; ++j){     
-                int required = -1*(nums[i] + nums[j]);    
-                if(hashMap.count(required) && hashMap.find(required)->second > j){ //If it exists in hashmap and its last occurrence index > 2nd fixed index, we found our triplet.
-                    answer.push_back({nums[i] , nums[j] , required});
-                }
-                j = hashMap.find(nums[j])->second; //Update j to last occurence of 2nd fixed number to avoid duplicate triplets.
-            }
-            i = hashMap.find(nums[i])->second;     //Update i to last occurence of 1st fixed number to avoid duplicate triplets.
+            
         }
-        return answer;  //Return answer vector.
+    
+
+        // Processing duplicates of Number 1
+        while (i + 1 < num.size() && num[i + 1] == num[i]) 
+            i++;
     }
+       return res; 
+    }
+    
 };
