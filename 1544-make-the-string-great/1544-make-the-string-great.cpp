8f8b4f88932f3d9574f5c0e1e45e00e9ba4d9d05
Ritik7Roshan <86067297+Ritik7Roshan@users.noreply.github.com>
2022-11-10 21:44:30 +0530

    Time: 14 ms (6.08%), Space: 7.4 MB (7.18%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,27 +1,18 @@
- class Solution {
+// Recursive Solution 
+class Solution {
 public:
     string makeGood(string s) {
-        // if s has less than 2 characters, we just return itself.
-        while (s.size() > 1) {
-            // 'find' records if we find any pair to remove.
-            bool find = false;
-            
-            // Check every two adjacent characters, currChar and nextChar.
-            for (int i = 0; i < s.size() - 1; ++i) {
-                char currChar = s[i], nextChar = s[i + 1];
-                
-                // If they make a pair, remove them from 's' and let 'find = true'.
-                if (abs(currChar - nextChar) == 32) {
-                    s = s.substr(0, i) + s.substr(i + 2);
-                    find = true;
-                    break;
-                }
-            }
-            
-            // If we cannot find any pair to remove, break the loop. 
-            if (!find)
-                break;
+        // If the string is empty,just return it.
+        if(s.empty())
+            return s;
+        //If we find a pair in s remove this pair from s 
+        //and solve the remaining string recursively
+        for(int i=0; i<s.size()-1; ++i)
+        {
+            if(abs(s[i]-s[i+1])==32)
+                return makeGood(s.substr(0,i)+s.substr(i+2));
         }
+        // Base case, if we cant find a pair,just return 's'
         return s;
     }
 };
