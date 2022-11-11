a24a8ad88a81bc4d0e7f9e767551adbb2884ae60
Ritik7Roshan <86067297+Ritik7Roshan@users.noreply.github.com>
2022-11-11 13:39:29 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (71.70%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,21 +1,23 @@
- class Solution {
+//Two pointer in place approach applicable only to C++ //language.
+class Solution {
 public:
     string makeGood(string s) {
-        // Use stack to store the visited characters.
-        vector<char> stack;
-        
-        // Iterate over 's'.
-        for (auto currChar : s) {
-            // If the current character make a pair with the last character in the stack,
-            // remove both of them. Otherwise, we add the current character to stack.
-            if (!stack.empty() && abs(stack.back() - currChar) == 32)
-                stack.pop_back(); 
-            else
-                stack.push_back(currChar);
+        int end=0;
+        for(int cur=0; cur<s.size(); ++cur)
+        {
+            //If s[cur] makes a pair with the last character s[end-1] in a good string remove s[end-1] by decrementing 'end' by 1.
+            // Otherwise,add s[curr] to the good string by overwritting s[end] by s[cur]
+            if(end>0 && abs(s[cur]-s[end-1])==32)
+                end--;
+            else{
+                s[end]=s[cur];
+                end++;
+            }
         }
-        
-        // Returns the string concatenated by all characters left in the stack.
-        string ans(stack.begin(), stack.end());
-        return ans;
+        //once the iteration ends, the string before end is the good string,
+        return s.substr(0,end);
     }
 };
+
+
+ 
