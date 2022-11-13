4b3f1904abb9d6b342402732d3f1203538ecb197
Ritik7Roshan <86067297+Ritik7Roshan@users.noreply.github.com>
2022-11-13 20:10:57 +0530

    Time: 151 ms (74.05%), Space: 54.8 MB (7.02%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+      set<int>s;
+        for(auto c:nums)
+        {
+            s.insert(c);
+        }
+        return(s.size()!=nums.size());
+    }
+};
