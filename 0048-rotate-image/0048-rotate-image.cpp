b33ef5cad76dfbb808d2cc61512e832f0e4d3264
Ritik7Roshan <86067297+Ritik7Roshan@users.noreply.github.com>
2023-03-06 19:10:10 +0530

    Time: 2 ms (56.11%), Space: 7 MB (76.39%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        // Taking Transpose Of A Matrix
-      int n=matrix.size();
-        for(int i=0; i<n; i++){
-            for(int j=i; j<n; j++){
+        int r=matrix.size();
+        int c=matrix[0].size();
+        
+        for(int i=0; i<r; i++)
+        {
+            for(int j=0; j<i; j++)
+            {
                 swap(matrix[i][j],matrix[j][i]);
             }
         }
-        for(int i=0; i<n; i++){
-            // Swap the 0th and last colmn using two pointer method
-            int left=0, right=n-1;
-            while(left<right){
-                swap(matrix[i][left],matrix[i][right]);
-                ++left;
-                --right;
-            }
-        }
+        for(int i=0; i<r; i++)
+            reverse(matrix[i].begin(),matrix[i].end());
     }
 };
