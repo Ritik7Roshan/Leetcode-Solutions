402e94da3fd34f32dcac8d8b8fa903cb86cc3bc5
Ritik7Roshan <86067297+Ritik7Roshan@users.noreply.github.com>
2023-03-10 20:49:44 +0530

    Time: 24 ms (44.23%), Space: 19.5 MB (80.52%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,19 +1,8 @@
+//Sorting TC=O(nLog n)
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        map<int,int>mpp;
-        int n=nums.size();
-        int ans;
-        for(auto v:nums)
-        {
-            mpp[v]++;
-        }
-        for(auto x:mpp)
-        {
-            if(x.second>(n/2))
-                ans=x.first;
-        }
-        return ans;
+        sort(nums.begin(),nums.end());
+        return nums[nums.size()/2];
     }
 };
-// TC O(log N)
