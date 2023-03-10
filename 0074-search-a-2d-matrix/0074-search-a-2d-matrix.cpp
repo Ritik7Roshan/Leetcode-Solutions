0a69c0cf94e8a5380296c74477991f1cffc5b82d
Ritik7Roshan <86067297+Ritik7Roshan@users.noreply.github.com>
2023-03-10 13:44:19 +0530

    Time: 6 ms (39.44%), Space: 9.3 MB (97.33%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,19 +1,35 @@
+// points to ponder 
+// n * m matrix convert to an array => matrix[x][y] => a[x * m + y]
+
+// an array convert to n * m matrix => a[x] =>matrix[x / m][x % m];
+
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) 
-    {
-       int rows=matrix.size();
-       int cols=matrix[0].size();
-        int row=0,col=cols-1;
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n=matrix.size();
+        int m=matrix[0].size();
         
-        while(row<rows && col>-1)
+        int l=0, h=m*n-1;
+        while(l!=h)
         {
-            if(matrix[row][col]==target) return true;
-            if(target<matrix[row][col]) col--;
-            else row++; 
+           int mid=(l+h-1)>>1;// doubt why (l+h-1)/2 why not (l+h)/2
+            if(matrix[mid/m][mid%m]>=target)
+                h=mid;
+            else
+                l=mid+1;
         }
-        return false;
+        return matrix[h/m][h%m]==target;
+        
+        //  int n = matrix.size();
+        // int m = matrix[0].size();
+        // int l = 0, r = m * n - 1;
+        // while (l != r){
+        //     int mid = (l + r - 1) >> 1;
+        //     if (matrix[mid / m][mid % m] < target)
+        //         l = mid + 1;
+        //     else 
+        //         r = mid;
+        // }
+        // return matrix[r / m][r % m] == target;
     }
 };
-//TC O(n)
-//SC O(1)
