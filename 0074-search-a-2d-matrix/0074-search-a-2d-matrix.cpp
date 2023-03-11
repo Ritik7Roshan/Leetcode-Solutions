35aa9164fb0f2aaa778504e9cf36568d5445c3ff
Ritik7Roshan <86067297+Ritik7Roshan@users.noreply.github.com>
2023-03-11 22:38:37 +0530

    Time: 5 ms (54.27%), Space: 9.6 MB (7.65%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -12,24 +12,12 @@ public:
         int l=0, h=m*n-1;
         while(l!=h)
         {
-           int mid=(l+h-1)>>1;// doubt why (l+h-1)/2 why not (l+h)/2
+           int mid=(l+h)>>1;// doubt why (l+h-1)/2 why not (l+h)/2
             if(matrix[mid/m][mid%m]>=target)
                 h=mid;
             else
                 l=mid+1;
         }
-        return matrix[h/m][h%m]==target;
-        
-        //  int n = matrix.size();
-        // int m = matrix[0].size();
-        // int l = 0, r = m * n - 1;
-        // while (l != r){
-        //     int mid = (l + r - 1) >> 1;
-        //     if (matrix[mid / m][mid % m] < target)
-        //         l = mid + 1;
-        //     else 
-        //         r = mid;
-        // }
-        // return matrix[r / m][r % m] == target;
+        return matrix[h/m][h%m]==target; 
     }
 };
