ad0fb7622e1ae4a53e7cd814fd26c93a06bfa04e
Ritik7Roshan <86067297+Ritik7Roshan@users.noreply.github.com>
2023-03-11 13:16:00 +0530

    Time: 16 ms (87.98%), Space: 19.5 MB (80.56%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,28 +1,18 @@
+// Boyre-Moore Voting Algorithm
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-       int n=nums.size();
-        int majority_element=0;
+        int count=0;
+        int candidate= 0;
         
-        for(int i=0; i<32; i++) {
-        int bit=1<<i;
-        
-        //count how many numbers have this bit set
-        int bit_count=0;
         for(int num: nums)
         {
-            if((num&bit)!=0)
+            if (count==0)
             {
-                bit_count++;
+                candidate =num;
             }
+            count+=(num == candidate )?1:-1;
         }
-        
-        //If this bit is present in more than n/2 elements then it must set in majority_element 
-        if(bit_count>n/2)
-        {
-            majority_element|=bit;
-        }
-    }
-        return majority_element;
+        return candidate;
     }
 };
