66224115c906bcccd9916abb7f01e45988ffa715
Ritik7Roshan <86067297+Ritik7Roshan@users.noreply.github.com>
2023-03-11 12:02:22 +0530

    Time: 19 ms (75.69%), Space: 19.5 MB (80.56%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,8 +1,28 @@
-//Sorting TC=O(nLog n)
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        return nums[nums.size()/2];
+       int n=nums.size();
+        int majority_element=0;
+        
+        for(int i=0; i<32; i++) {
+        int bit=1<<i;
+        
+        //count how many numbers have this bit set
+        int bit_count=0;
+        for(int num: nums)
+        {
+            if((num&bit)!=0)
+            {
+                bit_count++;
+            }
+        }
+        
+        //If this bit is present in more than n/2 elements then it must set in majority_element 
+        if(bit_count>n/2)
+        {
+            majority_element|=bit;
+        }
+    }
+        return majority_element;
     }
 };
