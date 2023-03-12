f11c5db9f8f472df79db606fcbe8ec5efa35c0ac
Ritik7Roshan <86067297+Ritik7Roshan@users.noreply.github.com>
2023-03-12 21:11:16 +0530

    Time: 112 ms (95.33%), Space: 23.9 MB (98.42%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,48 +1,41 @@
-class Solution {
+ class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& num) {
-        vector<vector<int> > res;
-
-    std::sort(num.begin(), num.end());
-
-    for (int i = 0; i < num.size(); i++) {
-        
-        int target = -num[i];
-        int front = i + 1;
-        int back = num.size() - 1;
-
-        while (front < back) {
-
-            int sum = num[front] + num[back];
-            
-            // Finding answer which start from number num[i]
-            if (sum < target)
-                front++;
-
-            else if (sum > target)
-                back--;
-
-            else {
-                vector<int> triplet = {num[i], num[front], num[back]};
-                res.push_back(triplet);
-                
-                // Processing duplicates of Number 2
-                // Rolling the front pointer to the next different number forwards
-                while (front < back && num[front] == triplet[1]) front++;
-
-                // Processing duplicates of Number 3
-                // Rolling the back pointer to the next different number backwards
-                while (front < back && num[back] == triplet[2]) back--;
-            }
-            
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin() , nums.end());    //Sorted Array
+        if(nums.size() < 3){    //Base case 1
+            return {};
         }
-    
-
-        // Processing duplicates of Number 1
-        while (i + 1 < num.size() && num[i + 1] == num[i]) 
-            i++;
+        if(nums[0] > 0){        //Base case 2
+            return {};
+        }
+        vector<vector<int>> answer;
+        for(int i = 0 ; i < nums.size() ; ++i){     //Traversing the array to fix the number.
+            if(nums[i] > 0){     //If number fixed is +ve, stop there because we can't make it zero by searching after it.
+                break;
+            }
+            if(i > 0 && nums[i] == nums[i - 1]){    //If number is getting repeated, ignore the lower loop and continue.
+                continue;
+            }
+            int low = i + 1 , high = nums.size() - 1;   //Make two pointers high and low, and initialize sum as 0.
+            int sum = 0;
+            while(low < high){                          //Search between two pointers, just similiar to binary search.
+                sum = nums[i] + nums[low] + nums[high];
+                if(sum > 0){   //If sum is +ve, means, we need more -ve numbers to make it 0, decreament high (high--).
+                    high--;
+                } else if(sum < 0){ //If sum is -ve, means, we need more +ve numbers to make it 0, increament low (low++).
+                    low++;
+                } else {
+                    answer.push_back({nums[i] , nums[low] , nums[high]});  //we have found the required triplet, push it in answer vector
+                    int last_low_occurence = nums[low] , last_high_occurence = nums[high];  //Now again, to avoid duplicate triplets, we have to navigate to last occurences of num[low] and num[high] respectively
+                    while(low < high && nums[low] == last_low_occurence){   // Update the low and high with last occurences of low and high.
+                        low++;
+                    }
+                    while(low < high && nums[high] == last_high_occurence){
+                        high--;
+                    }
+                }
+            }
+        }
+        return answer;      //Return the answer vector.
     }
-       return res; 
-    }
-    
-}; 
+};
