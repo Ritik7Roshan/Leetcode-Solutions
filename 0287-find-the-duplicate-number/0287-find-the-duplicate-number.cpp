5f523dc2ea1287d45fac86f8b1a0b04bf5f19b38
Ritik7Roshan <86067297+Ritik7Roshan@users.noreply.github.com>
2023-03-16 12:01:37 +0530

    Time: 195 ms (22.71%), Space: 83.9 MB (18.05%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,13 +1,16 @@
+//Using Set (donot fullfill ques condition)
 class Solution {
 public:
-    int findDuplicate(vector<int>& nums) 
-    {
-        sort(nums.begin(),nums.end());
-        for(auto i=1; i<nums.size(); i++)
+    int findDuplicate(vector<int>& nums) {
+       unordered_set<int> seen;
+        for(auto &num: nums)
         {
-            if(nums[i]==nums[i-1])
-                return nums[i];
+            if(seen.count(num))
+                return num;
+            seen.insert(num);
         }
         return -1;
     }
 };
+//TC O(N)
+//SC O(N)
