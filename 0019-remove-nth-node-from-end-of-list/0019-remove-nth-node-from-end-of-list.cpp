317f16bd1afc1a4f5aeae37844139c8ddef84c5a
Ritik7Roshan <86067297+Ritik7Roshan@users.noreply.github.com>
2023-03-18 00:01:31 +0530

    Time: 0 ms (100.00%), Space: 10.8 MB (40.02%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,25 +11,26 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        
-        // 2 kadam piche se delete kerna h toh kisiko 2 kadam aage bhj ke pata krlo kitna piche 
+         // 2 kadam piche se delete kerna h toh kisiko 2 kadam aage bhj ke pata krlo kitna piche 
         // rukna hai aur delete kerne k liye toh 1 kadam phele k node ka pointer badalna padta h
         // toh 1 kadam phlele se shuru kr do...ie dummy node.
         
         ListNode* dummy=new ListNode();
         dummy->next=head;
-        ListNode* fast=dummy;
-        ListNode* slow=dummy;
+        ListNode*fast=dummy;
+        ListNode*slow=dummy;
         
-        for(int i=1; i<=n; i++){
+        for(int i=1; i<=n; i++)
+        {
             fast=fast->next;
         }
-        
-        while(fast->next!=NULL){
+        while(fast->next!=NULL)
+        {
             fast=fast->next;
             slow=slow->next;
         }
         slow->next=slow->next->next;
         return dummy->next;
+        
     }
 };
