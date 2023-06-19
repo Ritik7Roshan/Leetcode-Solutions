c9f1f38cd7770fd972f4bdb06916dc66c9854019
Ritik7Roshan <86067297+Ritik7Roshan@users.noreply.github.com>
2023-06-19 23:22:01 +0530

    Time: 8 ms (30.52%), Space: 12.3 MB (20.97%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,46 +1,38 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) { 
-        int n=nums.size();
-        int i,l;
-        for(i=n-2; i>=0; i--)
-        {
-            if(nums[i]<nums[i+1])
-                break;
-        }
-        if(i<0)
-        {
-            reverse(nums.begin(),nums.end());
-        }
-        else
-        {
-        for(l=n-1; l>i; l--)
-        {
-            if(nums[l]>nums[i])
-                break;
-        }
-        swap(nums[i],nums[l]);
-        reverse(nums.begin()+i+1,nums.end());
-        }
-//         int n=nums.size(),i,l;
-//         for(i=n-2; i>=0; i--)
-//         {
-//              if(nums[i]<nums[i+1])
-//                  break;
-//         }
-//         if(i<0)
-//         {
-//             reverse(nums.begin(),nums.end());
-//         }
-//         else{
-//             for(l=n-1; l>i; l--)
-//             {
-//                 if(nums[l]>nums[i])
-//                     break;
-//             }
+    void nextPermutation(vector<int>& nums) {
         
-//             swap(nums[i],nums[l]);
-//             reverse(nums.begin()+i+1, nums.end());
-//     }
+        int n=nums.size()-1;
+        int infc=0;
+        // find infection point 
+        for(int i=n; i>0; i--)
+        {
+            if(nums[i]>nums[i-1])
+            {
+                infc=i;
+                break;
+            }
+        }
+        // if i am not abel to find any infec point means array given is already in descending order
+        if(infc==0)
+        {
+            sort(nums.begin(),nums.end());
+        }
+        else{
+            int toswap=nums[infc-1];
+            int mini=INT_MAX;
+            // search for next greater element
+            for(int j=infc; j<=n; j++)
+            {
+                if(nums[j]-toswap>0 && nums[j]-toswap<mini)
+                {
+                    int temp=nums[j];
+                    nums[j]=nums[infc-1];
+                    nums[infc-1]=temp;
+                }
+            }
+            // sort the remaining array
+            sort(nums.begin()+infc,nums.end());
+        }
     }
 };
