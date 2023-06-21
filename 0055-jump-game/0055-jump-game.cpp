253b046558bfe3f04fc90eb6170b93dc2d36fbdc
Ritik7Roshan <86067297+Ritik7Roshan@users.noreply.github.com>
2023-06-21 12:15:32 +0530

    Time: 558 ms (17.43%), Space: 50.6 MB (11.66%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,28 +1,34 @@
-// Memoization
+// Tabulation 
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        vector<int>dp(nums.size(),-1);
-        return reached(nums,0,dp);
-    }
-    bool reached(vector<int>&nums, int idx, vector<int>&dp)
-    {
+        int n=nums.size();
+        vector<int>dp(n,-1);
+        dp[n-1]=1;//base case
         
-        if(idx==nums.size()-1) return true;
-        if(nums[idx]==0) return false;
-        
-        int reach=idx+nums[idx];
-        
-        if(dp[idx]!=-1) return dp[idx];
-        
-        for(int jump=idx+1; jump<=reach; jump++)
+        for(int idx=n-2; idx>=0; idx--)
         {
-            if(jump<nums.size() && reached(nums,jump,dp)) 
-                return dp[jump]=true;// memoizing;
-                
+            if(nums[idx]==0)
+            {
+                dp[idx]=false;
+                continue;
+            }
+            
+            int flag=0;
+            int reach=idx+nums[idx];
+            for(int jump=idx+1; jump<=reach; jump++)
+            {
+                if(jump<nums.size() && dp[jump]){
+                    dp[idx]=true;
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag ==1)
+                continue;
+            
+            dp[idx]=false;
         }
-        return dp[idx]=false;//memoizing
+        return dp[0];
     }
 };
-//tc : O(N* N) -> for each index, I can have at max N jumps, hence O(N* N).
-// sc : O(N) + O(N) -> stack space plus dp array size.
