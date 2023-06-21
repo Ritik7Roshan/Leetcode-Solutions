27e3ef0f8fcb2e65c8ab18b68884a71388b63a14
Ritik7Roshan <86067297+Ritik7Roshan@users.noreply.github.com>
2023-06-21 13:59:11 +0530

    Time: 36 ms (91.11%), Space: 19 MB (78.02%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) 
-    {
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        
         vector<vector<int>>mergeIntervals;
-        if(intervals.size()==0)
-            return mergeIntervals;
+        int n=intervals.size();
         
         sort(intervals.begin(),intervals.end());
-//  after sorting intervals = [[1,3],[2,6],[8,10],[15,18]]
         
         mergeIntervals.push_back(intervals[0]);
-        int j=0;
-        //Traverse the whole vector takes O(N) time
         
-        for(int i=1; i<intervals.size(); i++)
+        int j=0;
+        for(int i=1; i<n; i++)
         {
-            //if results are overlapping 
             if(mergeIntervals[j][1]>=intervals[i][0])
-                mergeIntervals[j][1]=max(mergeIntervals[j][1],intervals[i][1]);
-            else 
+            {
+                mergeIntervals[j][1]=max(intervals[i][1],mergeIntervals[j][1]);  
+            }
+            else
             {
                 j++;
                 mergeIntervals.push_back(intervals[i]);
-            } 
+            }
         }
         return mergeIntervals;
-        
     }
 };
