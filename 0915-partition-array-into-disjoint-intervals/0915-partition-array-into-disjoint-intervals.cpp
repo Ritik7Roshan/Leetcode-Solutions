4af5ac9ddba9f7c95051fe16eb1e58b718f31f51
Ritik7Roshan <86067297+Ritik7Roshan@users.noreply.github.com>
2023-06-21 16:12:02 +0530

    Time: 163 ms (90.00%), Space: 94.2 MB (69.78%) - LeetHub

--- 0915-partition-array-into-disjoint-intervals/0915-partition-array-into-disjoint-intervals.cpp
+++ 0915-partition-array-into-disjoint-intervals/0915-partition-array-into-disjoint-intervals.cpp
@@ -1,26 +1,21 @@
-//using two traversal
+// Traversing once 
 class Solution {
 public:
     int partitionDisjoint(vector<int>& nums) {
-        
-        vector<int> minRight(nums.size());
-        minRight[nums.size()-1]=nums.back();
-        
-        int maxLeft=INT_MIN;
-        for(int i=nums.size()-2; i>=0; i--)// strt frm nums.size()-2
+       int globalMax=nums[0];
+        int localMax=nums[0];
+        int pi=0;
+        for(int i=1; i<nums.size(); i++)
         {
-            minRight[i]=min(nums[i],minRight[i+1]);
-            // last second compare with last wala therefore i+1
+            globalMax=max(globalMax,nums[i]);
+            if(localMax>nums[i])// means that num could also be included in left subarray therefore increase the partition
+            {
+                pi=i;
+                localMax=globalMax;
+            }
         }
-        
-        for(int i=0; i<nums.size()-1; i++)
-        {
-            maxLeft=max(maxLeft,nums[i]);
-            if(maxLeft<=minRight[i+1])
-                return i+1;
-        }
-         return -1;
+        return pi+1;
     }
 };
 //TC O(N)
-//SC O(N)
+//SC O(1)
