83e0994dfdbf44a37e3b7ce08eab416c818a6b92
Ritik7Roshan <86067297+Ritik7Roshan@users.noreply.github.com>
2023-06-21 15:59:11 +0530

    Time: 164 ms (88.48%), Space: 98.3 MB (43.04%) - LeetHub

--- 0915-partition-array-into-disjoint-intervals/0915-partition-array-into-disjoint-intervals.cpp
+++ 0915-partition-array-into-disjoint-intervals/0915-partition-array-into-disjoint-intervals.cpp
@@ -1,16 +1,12 @@
-//using three traversal
+//using two traversal
 class Solution {
 public:
     int partitionDisjoint(vector<int>& nums) {
-        vector<int> maxLeft(nums.size()),minRight(nums.size());
-        maxLeft[0]=nums[0];
+        
+        vector<int> minRight(nums.size());
         minRight[nums.size()-1]=nums.back();
         
-        for(int i=1; i<nums.size(); i++)
-        {
-            maxLeft[i]=max(nums[i],maxLeft[i-1]);
-        }
-        
+        int maxLeft=INT_MIN;
         for(int i=nums.size()-2; i>=0; i--)// strt frm nums.size()-2
         {
             minRight[i]=min(nums[i],minRight[i+1]);
@@ -19,7 +15,8 @@ public:
         
         for(int i=0; i<nums.size()-1; i++)
         {
-            if(maxLeft[i]<=minRight[i+1])
+            maxLeft=max(maxLeft,nums[i]);
+            if(maxLeft<=minRight[i+1])
                 return i+1;
         }
          return -1;
