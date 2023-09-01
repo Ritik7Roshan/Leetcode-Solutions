88cbab57fe25f648bb02dc89d4b64cae8183d447
Ritik7Roshan <86067297+Ritik7Roshan@users.noreply.github.com>
2023-09-01 23:59:19 +0530

    Time: 3 ms (89.82%), Space: 8.5 MB (32.26%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -2,13 +2,7 @@ class Solution {
 public:
     int numSetBits(int n)
     {
-        int cnt=0;
-        while(n)
-        {
-            cnt+=n&1;
-            n>>=1; 
-        }
-        return cnt;
+         return __builtin_popcount(n);
     }
     vector<int> countBits(int n) {
         vector<int> ans;
